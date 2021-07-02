3b269d781c381cb798ba638e0ccf9676e7cf86c0
Fagner da Silva <fdsfagner@gmail.com>
2021-07-02 07:52:58 -0300

    Create migration to players

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :challenge_rush, ChallengeRush.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "Phoenix1234",
   database: "challenge_rush_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :challenge_rush, ChallengeRush.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "Phoenix1234",
   database: "challenge_rush_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
